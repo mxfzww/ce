757970c5391b5f26020f34625247bf24d49c27e5
zw <2426595849@qq.com>
2020-12-28 23:40:45 +0800

    add:fix

--- app/Commands.php
+++ app/Commands.php
@@ -4,13 +4,15 @@
  * User: zhaowei
  * DateTime: 2020/12/28 下午7:23
  */
+
 namespace Zw\Hello;
 
 use Zw\Hello\Lib\Foo;
 
 class Commands
 {
-    public function getName(){
+    public function getName()
+    {
         (new Foo())->getName();
     }
 }
